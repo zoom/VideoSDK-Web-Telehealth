b79b4d8fefd773e94d140de86b6252e41d40a7ae
ekaansh arora <ekaansh.arora@zoom.us>
2024-03-18 16:33:14 +0530

    feat: videocall fixes & transcription card

--- src/components/Videocall.tsx
+++ src/components/Videocall.tsx
@@ -1,144 +1,125 @@
+import Link from "next/link";
+import RecordingModal from "./RecordingModal";
 import uitoolkit from "@zoom/videosdk-ui-toolkit";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { type MutableRefObject, useEffect, useRef, useState } from "react";
 import { Button } from "~/components/ui/button";
-import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 import { useToast } from "./ui/use-toast";
 import { LinkIcon } from "lucide-react";
-import ZoomVideo, {
-  type VideoPlayer,
-  RecordingStatus,
-  type RecordingClient,
-  type LiveTranscriptionClient,
-  type LiveTranscriptionMessage,
-  type VideoClient,
-} from "@zoom/videosdk";
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 import { Label } from "~/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { api } from "~/utils/api";
-import Link from "next/link";
+import { Card } from "./ui/card";
 import { Upload } from "lucide-react";
-import RecordingModal from "./RecordingModal";
+import { WorkAroundForSafari } from "~/utils/safari";
+import ZoomVideo, { RecordingStatus, VideoQuality } from "@zoom/videosdk";
+import type { VideoPlayer, RecordingClient, LiveTranscriptionClient, LiveTranscriptionMessage, VideoClient } from "@zoom/videosdk";
+import "@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css";
 
 const Videocall = (props: { jwt: string; session: string; isCreator: boolean }) => {
   const writeZoomSessionID = api.room.addZoomSessionId.useMutation();
   const isRender = useRef(0);
   const [incall, setIncall] = useState(false);
-  const previewContainer = useRef<HTMLDivElement>(null);
-  const sessionContainer = useRef<HTMLDivElement>(null);
+  const previewContainerRef = useRef<HTMLDivElement>(null);
+  const videoContainerRef = useRef<HTMLDivElement>(null);
   const { data } = useSession();
   const router = useRouter();
   const { toast } = useToast();
-
   const client = useRef(ZoomVideo.createClient());
   const transcriptionClient = useRef<typeof LiveTranscriptionClient>();
   const recordingClient = useRef<typeof RecordingClient>();
-  const [videoStarted, setVideoStarted] = useState(client.current.getCurrentUserInfo()?.bVideoOn);
-  const [audioStarted, setAudioStarted] = useState(client.current.getCurrentUserInfo() && client.current.getCurrentUserInfo().audio !== "");
-  const [isMuted, setIsMuted] = useState(client.current.getCurrentUserInfo()?.muted);
+  const [isVideoMuted, setIsVideoMuted] = useState(!client.current.getCurrentUserInfo()?.bVideoOn);
+  const [isAudioMuted, setIsAudioMuted] = useState(client.current.getCurrentUserInfo()?.muted);
   const [isStartedLiveTranscription, setIsStartedLiveTranscription] = useState(false);
-  const [transcriptionSubtitle, setTranscriptionSubtitle] = useState("");
+  const [transcriptionSubtitle, setTranscriptionSubtitle] = useState<Record<string, { name: string; text: string; isSelf: boolean }>>({});
   const [isRecording, setIsRecording] = useState(RecordingStatus.Stopped);
 
   useEffect(() => {
     if (isRender.current === 0) {
-      uitoolkit.openPreview(previewContainer.current!);
+      uitoolkit.openPreview(previewContainerRef.current!);
       isRender.current = 1;
     }
   }, []);
 
   const init = async () => {
-    //will need to set enforceMultipleVideos dynamically based on isSupportMultipleVideos
-    await client.current.init("en-US", "CDN", {enforceMultipleVideos: true});
-    try {
-      await client.current.join(props.session, props.jwt, data?.user.name ?? "User").catch((e) => {
-        console.log(e);
-      });
-      if (props.isCreator) {
-        await writeZoomSessionID.mutateAsync({ zoomSessionsId: client.current.getSessionInfo().sessionId, roomId: props.session });
-      }
-    } catch (e) {
+    await client.current.init("en-US", "Global", { patchJsMedia: true });
+    client.current.on("peer-video-state-change", (payload) => void renderVideo(payload));
+    await client.current.join(props.session, props.jwt, data?.user.name ?? "User").catch((e) => {
       console.log(e);
+    });
+    if (props.isCreator) {
+      await writeZoomSessionID.mutateAsync({ zoomSessionsId: client.current.getSessionInfo().sessionId, roomId: props.session });
     }
+
     recordingClient.current = client.current.getRecordingClient();
     transcriptionClient.current = client.current.getLiveTranscriptionClient();
-    console.log("recording status", isRecording);
-    client.current.on('peer-video-state-change', renderVideo)
   };
-  const renderVideo = async() => {
-    console.log('RENDERING VIDEO')
+
+  const renderVideo = async (event: { action: "Start" | "Stop"; userId: number }) => {
     const mediaStream = client.current.getMediaStream();
-    const videoPlayer = document.querySelector('video-player-container');
-    if (videoPlayer) {
-      videoPlayer.innerHTML = '';
+    if (event.action === "Stop") {
+      const element = await mediaStream.detachVideo(event.userId);
+      Array.isArray(element) ? element.forEach((el) => el.remove()) : element.remove();
+    } else {
+      const userVideo = await mediaStream.attachVideo(event.userId, VideoQuality.Video_360P);
+      videoContainerRef.current!.appendChild(userVideo as VideoPlayer);
     }
-    console.log('multiple', mediaStream.isSupportMultipleVideos())
-    for (const user of client.current.getAllUser()) {
-      if (user.bVideoOn) {
-        console.log(user.userId)
-        const userVideo = await mediaStream.attachVideo(user.userId, 1);
-        if (userVideo) document.querySelector("video-player-container")?.appendChild(userVideo as VideoPlayer);
-      } else {
-        console.log('detaching video')
-         await mediaStream.detachVideo(user.userId);
-
-      }
-    }
-  }
-
+  };
 
   const startCall = async () => {
     toast({ title: "Joining", description: "Please wait..." });
     await init();
-    uitoolkit.closePreview(previewContainer.current!);
-    renderVideo();
+    uitoolkit.closePreview(previewContainerRef.current!);
     setIncall(true);
+    const mediaStream = client.current.getMediaStream();
+    // @ts-expect-error https://stackoverflow.com/questions/7944460/detect-safari-browser/42189492#42189492
+    window.safari ? await WorkAroundForSafari(client.current) : await mediaStream.startAudio();
+    setIsAudioMuted(client.current.getCurrentUserInfo().muted);
+    await mediaStream.startVideo();
+    setIsVideoMuted(!client.current.getCurrentUserInfo().bVideoOn);
+    await renderVideo({ action: "Start", userId: client.current.getCurrentUserInfo().userId });
   };
 
+  // not sure about this yet
   const leaveCall = async () => {
-    try {
-      await client.current.leave(true);
-    } catch (e) {}
-    void router.push("/");
+    toast({ title: "Leaving", description: "Please wait..." });
+    client.current.off("peer-video-state-change", () => void renderVideo);
+    await client.current.leave().catch((e) => console.log("leave error", e));
+    setIncall(false);
+    await router.push("/", undefined, { shallow: false });
   };
 
   const onCameraClick = async () => {
     const mediaStream = client.current.getMediaStream();
-    if (videoStarted) {
-      await mediaStream.stopVideo();
-      renderVideo();
-      setVideoStarted(false);
+    if (isVideoMuted) {
+      await mediaStream.startVideo();
+      setIsVideoMuted(false);
+      await renderVideo({ action: "Start", userId: client.current.getCurrentUserInfo().userId });
     } else {
-      await mediaStream.startVideo().then(() => {
-        renderVideo();
-      });
-      setVideoStarted(true);
+      await mediaStream.stopVideo();
+      setIsVideoMuted(true);
+      await renderVideo({ action: "Stop", userId: client.current.getCurrentUserInfo().userId });
     }
   };
 
   const onMicrophoneClick = async () => {
-    //adjust button to change wording with mute/unmute
     const mediaStream = client.current.getMediaStream();
-    if (audioStarted) {
-      if (isMuted) {
-        await mediaStream?.unmuteAudio();
-      } else {
-        await mediaStream?.muteAudio();
-      }
-      setIsMuted(client.current.getCurrentUserInfo()?.muted);
-    } else {
-      await mediaStream?.startAudio();
-      setAudioStarted(true);
-    }
+    isAudioMuted ? await mediaStream?.unmuteAudio() : await mediaStream?.muteAudio();
+    setIsAudioMuted(client.current.getCurrentUserInfo()?.muted);
   };
-  //create nice captions + disable button if audio is not started
+
   const onTranscriptionClick = async () => {
     const handleCaptions = (payload: LiveTranscriptionMessage) => {
-      console.log(`${payload.displayName} said: ${payload.text}`);
-      setTranscriptionSubtitle(payload.text);
+      setTranscriptionSubtitle(
+        (prev) =>
+          (prev = {
+            ...prev,
+            [payload.msgId]: { name: payload.displayName, text: payload.text, isSelf: payload.userId === client.current.getCurrentUserInfo().userId },
+          })
+      );
     };
 
     if (transcriptionClient.current === undefined) return;
@@ -169,40 +150,67 @@ const Videocall = (props: { jwt: string; session: string; isCreator: boolean }) 
 
   return (
     <>
-      <div id="meeting" className={incall ? "mb-8 mt-8 flex flex-1" : "hidden"} ref={sessionContainer} />
-      {!incall ? (
-        <>
-          <div id="preview" className="mb-8 mt-8 flex flex-1" ref={previewContainer} />
-          <div className="mx-auto flex w-64 self-center">
-            <Button className="flex flex-1" onClick={startCall}>
-              Join
-            </Button>
-            <div className="w-4"></div>
-            <Button
-              variant={"outline"}
-              className="flex flex-1"
-              onClick={async () => {
-                const link = `${window.location.toString()}`;
-                await navigator.clipboard.writeText(link);
-                toast({ title: "Copied link to clipoard", description: link });
-              }}
-            >
-              Copy Link
-              <LinkIcon height={16} />
-            </Button>
+      <div id="meeting" className={incall ? "mb-8 mt-8 flex flex-1" : "hidden"} />
+      <div className="flex h-full w-full flex-1 flex-col p-8">
+        {/* @ts-expect-error html component */}
+        <video-player-container ref={videoContainerRef}></video-player-container>
+        {!incall ? (
+          <>
+            <div id="preview" className="mb-8 mt-8 flex flex-1 self-center" ref={previewContainerRef} />
+            <div className="mx-auto flex w-64 self-center">
+              <Button className="flex flex-1" onClick={startCall}>
+                Join
+              </Button>
+              <div className="w-4"></div>
+              <Button
+                variant={"outline"}
+                className="flex flex-1"
+                onClick={async () => {
+                  const link = `${window.location.toString()}`;
+                  await navigator.clipboard.writeText(link);
+                  toast({ title: "Copied link to clipoard", description: link });
+                }}
+              >
+                Copy Link
+                <LinkIcon height={16} />
+              </Button>
+            </div>
+          </>
+        ) : (
+          <div className="flex w-full flex-1 flex-col justify-around self-center">
+            <div className="mt-8 flex w-[50%] flex-1 justify-around self-center">
+              <Button onClick={onCameraClick}>{`${isVideoMuted ? "Unmute Camera" : "Mute Camera"}`}</Button>
+              <Button onClick={onMicrophoneClick}>{`${isAudioMuted ? "Unmute Mic" : "Mute Mic"}`}</Button>
+              <Button onClick={onTranscriptionClick}>{`${isStartedLiveTranscription ? "stop transcription" : "start transcription"}`}</Button>
+              <Button onClick={onRecordingClick}>{`${isRecording === RecordingStatus.Recording ? "stop recording" : "start recording"}`}</Button>
+              <Button variant={"outline"} onClick={leaveCall}>
+                Leave
+              </Button>
+            </div>
+            {Object.keys(transcriptionSubtitle).length > 0 ? (
+              <Card className="mt-8 flex max-h-[200px] flex-col self-center overflow-y-scroll rounded-sm bg-white p-4 shadow-sm">
+                {Object.keys(transcriptionSubtitle).map((key) => (
+                  <p
+                    key={key}
+                    className="text-sm text-gray-600"
+                    style={
+                      transcriptionSubtitle[key]?.isSelf
+                        ? {
+                            alignSelf: "flex-end",
+                          }
+                        : {}
+                    }
+                  >
+                    <span className="font-bold">{transcriptionSubtitle[key]?.name}</span>: {transcriptionSubtitle[key]?.text}
+                  </p>
+                ))}
+              </Card>
+            ) : (
+              <></>
+            )}
           </div>
-        </>
-      ) : (
-        <div>
-          {/* @ts-expect-error html component */}
-          <video-player-container></video-player-container>
-          <Button onClick={onCameraClick}>{`${videoStarted ? "stop camera" : "start camera"}`}</Button>
-          <Button onClick={onMicrophoneClick}>{`${audioStarted ? (isMuted ? "unMute" : "Mute") : "start audio"}`}</Button>
-          <Button onClick={onTranscriptionClick}>{`${isStartedLiveTranscription ? "stop transcription" : "start transcription"}`}</Button>
-          <Button onClick={onRecordingClick}>{`${isRecording === RecordingStatus.Recording ? "stop recording" : "start recording"}`}</Button>
-          <p>{transcriptionSubtitle}</p>
-        </div>
-      )}
+        )}
+      </div>
       <br />
       {incall && <ActionModal client={client} />}
       {incall && <SettingsModal client={client} />}
@@ -312,8 +320,6 @@ const SettingsModal = (props: { client: MutableRefObject<typeof VideoClient> }) 
       await mediaStream.switchSpeaker(speaker.deviceId);
     }
   };
-
-  console.log("camera", micList);
 
   return (
     <Dialog>
