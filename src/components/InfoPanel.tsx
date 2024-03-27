bc330c06dc8dcd921312041c4c6c1006a3e61bf0
Ekaansh Arora <ekaansh68@gmail.com>
2024-03-27 20:51:08 +0700

    fix: types

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,78 +1,49 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "~/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "~/components/ui/card";
 
 interface FeatureProps {
- 
   title: string;
   description: string;
 }
 
 const features: FeatureProps[] = [
   {
-   
     title: "Differentiated Views",
-    description:
-      "Experience our telehealth sample application as both a doctor and a patient with seeded test data",
+    description: "Experience our telehealth sample application as both a doctor and a patient with seeded test data",
   },
   {
-   
     title: "Plug-and-Play",
-    description:
-      "Customize color schemes, section headers, descriptions, and more as you see fit",
+    description: "Customize color schemes, section headers, descriptions, and more as you see fit",
   },
   {
-    
     title: "Integrated Zoom Technology",
-    description:
-      "Gain access to a personlized video call experience with the Video SDK",
+    description: "Gain access to a personlized video call experience with the Video SDK",
   },
   {
-   
     title: "Additional Point",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
   },
 ];
 const InfoPanel = () => {
   return (
-<section
-      id="howItWorks"
-      className="container text-center py-24 sm:py-32"
-    >
-      <h2 className="text-3xl md:text-4xl font-bold ">
-        How It{" "}
-        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          Works{" "}
-        </span>
+    <section id="howItWorks" className="container py-24 text-center sm:py-32">
+      <h2 className="text-3xl font-bold md:text-4xl ">
+        How It <span className="bg-gradient-to-b from-primary/60 to-primary bg-clip-text text-transparent">Works </span>
         Step-by-Step Guide
       </h2>
-      <p className="md:w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground">
-       Experience The Power Zoom's Video SDK Offers
-      </p>
+      <p className="mx-auto mb-8 mt-4 text-xl text-muted-foreground md:w-3/4">Experience The Power Zoom&apos;s Video SDK Offers</p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
         {features.map(({ title, description }: FeatureProps) => (
-          <Card
-            key={title}
-            className="bg-muted/50"
-          >
+          <Card key={title} className="bg-muted/50">
             <CardHeader>
-              <CardTitle className="grid gap-4 place-items-center">
-                {title}
-              </CardTitle>
+              <CardTitle className="grid place-items-center gap-4">{title}</CardTitle>
             </CardHeader>
             <CardContent>{description}</CardContent>
           </Card>
         ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default InfoPanel;
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,49 +3,39 @@ import Image from "next/image";
 import { buttonVariants } from "~/components/ui/button";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from "~/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "~/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import InfoPanel from "./InfoPanel";
 
 const LandingPage = () => {
   return (
-    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-      <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
+    <section className="container grid place-items-center gap-10 py-20 md:py-32 lg:grid-cols-2">
+      <div className="space-y-6 text-center lg:text-start">
+        <main className="text-5xl font-bold md:text-6xl">
           <h1 className="inline">
-          <span className="flex justify-start">
-            <Image className="inline" src={"/logo.svg"} height={34} width={150} alt="product logo" />
-            <h1 className="ml-2 inline text-5xl font-bold leading-none text-gray-700">Telehealth</h1>
-          </span>{" "}
-          <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              Starter-Kit
-            </span>
+            <span className="flex justify-start">
+              <Image className="inline" src={"/logo.svg"} height={34} width={150} alt="product logo" />
+              <h1 className="ml-2 inline text-5xl font-bold leading-none text-gray-700">Telehealth</h1>
+            </span>{" "}
+            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] bg-clip-text text-transparent">Starter-Kit</span>
           </h1>{" "}
         </main>
 
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-        This is a sample app for using the Zoom Video SDK for TeleHealth use-case
-        </p>
+        <p className="mx-auto text-xl text-muted-foreground md:w-10/12 lg:mx-0">This is a sample app for using the Zoom Video SDK for TeleHealth use-case</p>
 
-        <div className="space-y-4 md:space-y-0 md:space-x-4">
-        <Button variant={"outline"} onClick={() => void signIn()}>
+        <div className="space-y-4 md:space-x-4 md:space-y-0">
+          <Button variant={"outline"} onClick={() => void signIn()}>
             Sign in
           </Button>
           <a
-            href="https://github.com/zoom/" target="_blank"
+            href="https://github.com/zoom/"
+            target="_blank"
             className={`w-full md:w-1/3 ${buttonVariants({
               variant: "outline",
             })}`}
           >
             Github Repository
-            <GitHubLogoIcon className="ml-2 w-5 h-5" />
+            <GitHubLogoIcon className="ml-2 h-5 w-5" />
           </a>
         </div>
       </div>
@@ -57,32 +47,26 @@ const LandingPage = () => {
       //   </Button> */}
 
       <div className="z-10">
-        <div className="hidden lg:flex flex-row flex-wrap gap-8 relative w-[700px] h-[500px]">
-          <Card className="absolute top-[150px] left-[50px] w-72  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
-              <CardHeader className="flex flex-row items-center gap-4 pb-2">
-                <Avatar>
-                  <AvatarImage
-                    alt=""
-                    src=""
-                  />
-                  <AvatarFallback>DV</AvatarFallback>
-                </Avatar>
-
-                <div className="flex flex-col">
-                  <CardTitle className="text-lg">Doctor</CardTitle>
-                  <CardDescription>Doctor Experience</CardDescription>
-                </div>
-              </CardHeader>
-              <CardContent>Walk through as a Doctor</CardContent>
-            </Card>
-
-          <Card className="absolute top-[150px] right-[50px] w-72  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <div className="relative hidden h-[500px] w-[700px] flex-row flex-wrap gap-8 lg:flex">
+          <Card className="absolute left-[50px] top-[150px] w-72  shadow-black/10 drop-shadow-xl dark:shadow-white/10">
             <CardHeader className="flex flex-row items-center gap-4 pb-2">
               <Avatar>
-                <AvatarImage
-                  alt=""
-                  src=""
-                />
+                <AvatarImage alt="" src="" />
+                <AvatarFallback>DV</AvatarFallback>
+              </Avatar>
+
+              <div className="flex flex-col">
+                <CardTitle className="text-lg">Doctor</CardTitle>
+                <CardDescription>Doctor Experience</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>Walk through as a Doctor</CardContent>
+          </Card>
+
+          <Card className="absolute right-[50px] top-[150px] w-72  shadow-black/10 drop-shadow-xl dark:shadow-white/10">
+            <CardHeader className="flex flex-row items-center gap-4 pb-2">
+              <Avatar>
+                <AvatarImage alt="" src="" />
                 <AvatarFallback>PV</AvatarFallback>
               </Avatar>
 
@@ -97,7 +81,7 @@ const LandingPage = () => {
       </div>
       <div className="shadow"></div>
     </section>
-  )
-}
+  );
+};
 
 export default LandingPage;
