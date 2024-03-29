31a2abc51dd7060507766135a9a2fd8473a80b2a
Rehema <rehema.armorer@zoom.us>
2024-03-29 10:24:03 -0400

    changes to doctir view

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import { Button } from "./button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+
 
 function Header() {
   const { status } = useSession();
 
   return (
     <div className="w-screen bg-white px-4 lg:px-8">
+      
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <span className="my-8 flex justify-start">
+          <Image className="inline" src={"/logo.svg"} height={20} width={100} alt="product logo" />
+        </span>
         <div className="ml-auto flex gap-2">
           <Link
             className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
