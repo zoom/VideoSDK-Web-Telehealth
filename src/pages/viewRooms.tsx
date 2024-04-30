b58c7fbf061a7d0ed2f8a1fda7fb1bd0567d0895
Rehema <rehema.armorer@zoom.us>
2024-04-30 09:18:01 -0400

    wip viewrooms UI

--- src/pages/viewRooms.tsx
+++ src/pages/viewRooms.tsx
@@ -9,6 +9,9 @@ import { Skeleton } from "~/components/ui/skeleton";
 import { api } from "~/utils/api";
 import Footer from "~/components/ui/footer";
 import Header from "~/components/ui/header";
+import { Avatar } from "~/components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+
 
 export default function Home() {
   const { data: createdRoomsPast, isLoading: loading1 } = api.room.getCreatedPast.useQuery();
@@ -19,40 +22,21 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex h-screen w-screen flex-col items-center overflow-y-scroll bg-gray-100 pb-4">
-        <h1 className="mb-4 mt-6 flex text-center text-5xl font-bold leading-none text-gray-700">Rooms</h1>
-        <h3 className="gray text-left text-2xl font-bold text-gray-700">Created</h3>
+      <div className="flex h-screen w-screen flex-col items-left overflow-y-scroll bg-gray-100 pb-4 px-10">
+        <h1 className="mb-4 mt-6 flex text-center text-4xl font-sans leading-none text-gray-700">Your Schedule</h1>
+        <h3 className="gray text-left text-xl font-sans text-gray-700">Upcoming Appointments</h3>
         <div>
-          <Tabs defaultValue="account" className="mt-2 flex w-full flex-col self-center">
-            <TabsList>
-              <TabsTrigger value="password">Past</TabsTrigger>
-              <TabsTrigger value="account">Upcoming</TabsTrigger>
-            </TabsList>
-            <TabsContent value="password">
+    
               <Rooms rooms={createdRoomsPast} isLoading={loading1} />
-            </TabsContent>
-            <TabsContent value="account">
-              <Rooms rooms={createdRoomsUpcoming} isLoading={loading2} />
-            </TabsContent>
-          </Tabs>
+
         </div>
-        <h3 className="text-left text-xl font-bold text-gray-700">Invited</h3>
+        <h3 className="text-left text-xl font-sans text-gray-700">Past Appointments</h3>
         <div>
-          <Tabs defaultValue="account" className="mt-2 flex w-full flex-col self-center">
-            <TabsList>
-              <TabsTrigger value="password">Past</TabsTrigger>
-              <TabsTrigger value="account">Upcoming</TabsTrigger>
-            </TabsList>
-            <TabsContent value="password">
               <Rooms rooms={invitedRoomsPast} isLoading={loading3} />
-            </TabsContent>
-            <TabsContent value="account">
-              <Rooms rooms={invitedRoomsUpcoming} isLoading={loading4} />
-            </TabsContent>
-          </Tabs>
         </div>
+    
         <Link href="/">
-          <Button variant={"link"}>back</Button>
+          <Button variant={"link"}>back to dashboard</Button>
         </Link>
       </div>
       <Footer />
@@ -62,8 +46,11 @@ export default function Home() {
 
 const Rooms = ({ rooms, isLoading }: { rooms?: Room[]; isLoading: boolean }) => {
   const { data } = useSession();
+  rooms?.forEach((room) => {
+    console.log('room', room)
+  })
   return (
-    <div className="mb-4 flex flex-wrap justify-center">
+    <div className="mb-4 flex">
       {isLoading ? (
         <Card className="m-8 flex h-[28rem] w-80 flex-col justify-center self-center rounded-lg bg-white p-5 text-center shadow-lg">
           <Skeleton className="h-96 w-full animate-pulse" />
@@ -72,9 +59,32 @@ const Rooms = ({ rooms, isLoading }: { rooms?: Room[]; isLoading: boolean }) => 
         <Card className="m-4 flex min-h-64 w-64 flex-col justify-center self-center rounded-lg bg-white p-5 text-center shadow-lg">No Rooms</Card>
       ) : (
         rooms?.map((room) => (
-          <Card key={room.id} className="mx-6 my-4 flex h-[28rem] w-80 flex-col self-center rounded-lg bg-white p-5 text-center shadow-lg">
-            <UpcomingSession data={room} isDoctor={data?.user.role === "doctor"} key={room.id} />
-          </Card>
+          // <Card key={room.id} classNameName="mx-6 my-4 flex h-[28rem] w-80 flex-col self-center rounded-lg bg-white p-5 text-center shadow-lg">
+          //           <div className="pr-4 bg-blue-200 p-2 rounded-lg text-center">
+          //             <p className="text-4xl font-bold text-white">18th</p>
+          //             <p className="text-sm text-white">November, 2023</p>
+          //           </div>
+          //   <UpcomingSession data={room} isDoctor={data?.user.role === "doctor"} key={room.id} />
+        
+          // </Card>
+          <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5">
+          <div className="p-8 flex items-center">
+              <div className="pr-4 bg-blue-200 p-2 rounded-lg text-center">
+              <p className="text-4xl font-bold text-white">{room.time.toString().slice(4,11)}</p>
+              </div>
+              <div className="ml-4">
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{room.title}</div>
+              <p className="mt-2 text-gray-500">{room.time.toString().slice(16, 21)} - </p>
+              <p className="mt-2 text-gray-500">Dr. {room.User_CreatedBy?.name}, Cardiology Specialist</p>
+              <Avatar className="ml-4">
+                <AvatarImage src={data?.user.image as string | undefined} alt="User Avatar" />
+              </Avatar>              
+              <p className="mt-2 text-gray-500">Patient: {room.User_CreatedFor?.[0].name}</p>
+              {/* <img className="h-16 w-16 rounded-full mx-auto" src="https://randomuser.me/api/portraits/women/50.jpg" alt="Patient's Image"> */}
+              {/* <UpcomingSession data={room} isDoctor={data?.user.role === 'doctor'} key={room.id} /> */}
+              </div>
+          </div>
+          </div>
         ))
       )}
     </div>
