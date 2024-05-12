1e148c131ecb4939069f1828d14ceda57b0aa9ed
Ekaansh Arora <ekaansh68@gmail.com>
2024-05-13 00:15:32 +0530

    feat: demo mode tool tip

--- src/components/ui/ToggleRole.tsx
+++ src/components/ui/ToggleRole.tsx
@@ -1,14 +1,28 @@
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from "~/components/ui/tooltip";
 import { useSession } from "next-auth/react";
 import { Button } from "~/components/ui/button";
 import { capitalize } from "~/lib/utils";
 import { api } from "~/utils/api";
+import { LucideInfo } from "lucide-react";
 
 const ToggleRoleBanner = () => {
   const { data, update } = useSession();
   const toggleRole = api.user.toggleRole.useMutation();
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-yellow-200 py-4 ">
-      <p className="text-center text-xl font-bold text-slate-800">Demo Mode</p>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex items-center justify-center">
+              <p className="text-center text-xl font-bold text-slate-800">Demo Mode</p> <LucideInfo className="ml-2" />
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Demo mode is enabled, we hide your personal details and add mock data to make it easier understand how the app works.</p>
+            <p>You can toggle the account role between a Doctor and a Patient.</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       {data?.user.role ? (
         <>
           <p className="text-center">You are currently logged in as a {capitalize(data?.user.role as string)}.</p>
