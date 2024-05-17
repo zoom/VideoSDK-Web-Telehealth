a3705e8369650ee34e5c4f791c6f85855c521be8
Ekaansh Arora <ekaansh68@gmail.com>
2024-05-17 18:08:50 +0530

    feat: update toggle role UI

--- src/components/ui/ToggleRole.tsx
+++ src/components/ui/ToggleRole.tsx
@@ -7,24 +7,30 @@ const ToggleRoleBanner = () => {
   const { data, update } = useSession();
   const toggleRole = api.user.toggleRole.useMutation();
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-yellow-200 py-4 ">
-      <p className="text-center text-xl font-bold text-slate-800">Demo Mode</p>
-      {data?.user.role ? (
-        <>
-          <p className="text-center">You are currently logged in as a {capitalize(data?.user.role as string)}.</p>
-          <Button
-            className="flex self-center"
-            onClick={async () => {
-              await toggleRole.mutateAsync();
-              await update();
-            }}
-          >
-            View as {capitalize(data?.user.role === "doctor" ? "patient" : "doctor")}
-          </Button>
-        </>
-      ) : (
-        <></>
-      )}
+    <div className="z-50 flex h-full w-full flex-col items-center justify-center  bg-gray-900 py-4 text-white ">
+      <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
+        <p className="text-sm font-medium">
+          <span className="text-xl font-bold">Demo Mode</span>
+        </p>
+        <div className="flex items-center space-x-4">
+          {data?.user.role ? (
+            <>
+              <p className="text-center">Viewing as a {capitalize(data?.user.role as string)}</p>
+              <Button
+                className="flex self-center"
+                onClick={async () => {
+                  await toggleRole.mutateAsync();
+                  await update();
+                }}
+              >
+                View as {capitalize(data?.user.role === "doctor" ? "patient" : "doctor")}
+              </Button>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
