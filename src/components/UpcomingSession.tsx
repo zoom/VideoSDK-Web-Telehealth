3f75185bc0fe9e156458fc86ce5a84fda76f8dcb
mdh <michael.harrington@zoom.us>
2024-05-24 13:04:33 -0600

    blah blah blah blah blah

--- src/components/UpcomingSession.tsx
+++ src/components/UpcomingSession.tsx
@@ -20,126 +20,139 @@ const UpcomingSession = ({ data, isDoctor }: { data: RoomData; isDoctor?: boolea
   const deleteRoom = api.room.delete.useMutation();
   const utils = api.useUtils();
 
-  //convert to minutes to be able to check and conditionally render join session button 
-  console.log(moment(rooms.time).local().fromNow())
-  
-return (  
-   
-<div>
-  <div className="min-w-[680px] min-h-[8rem] bg-white shadow-lg rounded-lg w-full">
-   <div className="flex items-start pt-2" >
-   <p className="px-4 bg-blue-200 p-2 rounded-lg text-center text-1xl font-bold text-white mx-5">{rooms.time.toString().slice(4,7)}<br></br>{rooms.time.toString().slice(7,11)}</p>      
-   <div className="min-w-[540px] bg-white">
-      <div className="flex justify-start">
-         <Button
-            variant={"link"}
-            onClick={async () => {
-              const link = `${window.location.origin}/room/`;
-              await navigator.clipboard.writeText(link);
-              toast({ title: "Copied link to clipoard", description: link });
-            }}
-            className="max-w-full"
-          >
-            <p className="uppercase tracking-wide text-base text-indigo-500 font-semibold">{rooms.title}, with {isDoctor ? rooms.User_CreatedFor?.[0].name : rooms.User_CreatedBy?.name}</p>
-            <LinkIcon height={18} strokeWidth={3} />
-          </Button>            
-          <small className="text-sm text-gray-700">{moment(rooms.time).local().fromNow()}</small>
-         </div>
-         <Link className="flex flex-1 text-sm text-blue-500 hover:underline" href={`/room/${rooms.id}`}>
+  //convert to minutes to be able to check and conditionally render join session button
+  console.log(moment(rooms.time).local().fromNow());
+
+  return (
+    <div>
+      <div className="min-h-[8rem] w-full min-w-[680px] rounded-lg bg-white shadow-lg">
+        <div className="flex items-start gap-4 p-4">
+          <p className="text-1xl rounded-lg bg-blue-200 p-2 px-4 text-center font-bold text-white">
+            {rooms.time.toString().slice(4, 7)}
+            <br></br>
+            {rooms.time.toString().slice(7, 11)}
+          </p>
+          <div className="min-w-[540px] bg-white">
+            <div className="flex items-center justify-between">
+              <span className="flex items-center">
+                <h4 className="text-base font-semibold uppercase tracking-wide text-indigo-500">
+                  {rooms.title}, with {isDoctor ? rooms.User_CreatedFor?.[0].name : rooms.User_CreatedBy?.name}
+                </h4>
+                <Button
+                  variant={"link"}
+                  onClick={async () => {
+                    const link = `${window.location.origin}/room/`;
+                    await navigator.clipboard.writeText(link);
+                    toast({ title: "Copied link to clipoard", description: link });
+                  }}
+                  className="max-w-full"
+                >
+                  <LinkIcon height={18} strokeWidth={3} />
+                </Button>
+              </span>
+              <small className="text-sm text-gray-700">{moment(rooms.time).local().fromNow()}</small>
+            </div>
+            <Link className="flex flex-1 text-sm text-blue-500 hover:underline" href={`/room/${rooms.id}`}>
               Join Session
             </Link>
-         <p className="flex justify-start mt-3 text-gray-700 text-sm">
-         { /* automate doctor speciality */}
-         {isDoctor ? 
-            <Link href={`/viewPatient/${rooms.User_CreatedFor?.[0].id}`} className="text-gray-500">
-              <p className="text-grey-500 hover:text-blue-500" title='click for patient details'>Patient: {rooms.User_CreatedFor?.[0].name}</p>
-            </Link>
-            :  
-            <p className="text-gray-500">Dr. {rooms.User_CreatedBy?.name}, Cardiology Specialist</p> 
-          }
-         </p>
-         <div className="">
-         <Link href={`/viewNotes/`}>
-                <Button variant={"link"}>Notes</Button>
+            <p className="mt-3 flex justify-start text-sm text-gray-700">
+              {/* automate doctor speciality */}
+              {isDoctor ? (
+                <Link href={`/viewPatient/${rooms.User_CreatedFor?.[0].id}`} className="text-gray-500">
+                  <p className="text-grey-500 hover:text-blue-500" title="click for patient details">
+                    Patient: {rooms.User_CreatedFor?.[0].name}
+                  </p>
+                </Link>
+              ) : (
+                <p className="text-gray-500">Dr. {rooms.User_CreatedBy?.name}, Cardiology Specialist</p>
+              )}
+            </p>
+            <div className="flex gap-4">
+              <Link href={`/viewNotes/`}>
+                <Button className="p-0" variant={"link"}>
+                  Notes
+                </Button>
               </Link>
               <Link href={`/viewNotes/`}>
-                <Button variant={"link"}>Recordings</Button>
+                <Button className="p-0" variant={"link"}>
+                  Recordings
+                </Button>
               </Link>
-         </div>
+            </div>
+          </div>
+        </div>
       </div>
-   </div>
-</div>
-</div>
+    </div>
 
-        // <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5">
-        //   <div className="mb-2 mt-4 flex flex-1 flex-row justify-center">
-        //     <Link className="flex w-full flex-1 text-sm text-blue-500 hover:underline px-2" href={`/room/${rooms.id}`}>
-        //       Join Session
-        //     </Link>
-        //   </div>
-        //   <button
-        //     // variant={"destructive"}
-        //     onClick={async () => {
-        //       toast({ title: "Deleting Room", description: rooms.title });
-        //       await deleteRoom.mutateAsync({ id: rooms.id });
-        //       await utils.room.invalidate();              
-        //     }}
-        //     title="Cancel appointment"
-        //     className="flex w-full flex-1 text-sm text-blue-500 hover:underline px-2"
-        //   >
-        //    Delete/Cancel Session
-        //   </button>
-        //   <div className="p-8 flex items-center">
-        //       <div className="items-start">
-        //       <p className=" pr-4 bg-blue-200 p-2 rounded-lg text-center text-4xl font-bold text-white">{rooms.time.toString().slice(4,11)}</p>
-        //       <Link href={`/viewNotes/${rooms.id}`}>
-        //         <Button variant={"link"}>Notes</Button>
-        //       </Link>
-        //       <RecordingModal roomId={rooms.id} />
-        //       </div>
-              
-        //       <div className="mb-2 mt-4 flex flex-1 flex-row justify-center">
-              
-        //     </div>
-        //       <div className="ml-4">
-        //        <Button
-        //     variant={"link"}
-        //     onClick={async () => {
-        //       const link = `${window.location.origin}/room/${rooms.id}`;
-        //       await navigator.clipboard.writeText(link);
-        //       toast({ title: "Copied link to clipoard", description: link });
-        //     }}
-        //     className="max-w-full"
-        //   >
-        //     <span className="uppercase tracking-wide text-base text-indigo-500 font-semibold">{rooms.title}</span>
-        //     <LinkIcon height={18} strokeWidth={3} />
-        //   </Button>
-        //       <p className="text-sm ">{moment(rooms.time).local().fromNow()}</p>
-        //       <p className="mt-2 text-gray-500">{rooms.duration} hour(s)</p>
-        //       <p className="mt-2 text-gray-500">Dr. {rooms.User_CreatedBy?.name}, Cardiology Specialist</p>
-        //       {/* <Avatar className="ml-4">
-        //         <AvatarImage src={data?.user.image as string | undefined} alt="User Avatar" />
-        //       </Avatar>               */}
-        //         <Link href={`/viewPatient/${rooms.User_CreatedFor?.[0].id}`} className="mt-2 text-gray-500">
-        //         <p className="mt-2 text-grey-500 hover:text-blue-500" title='click for patient details'>Patient: {rooms.User_CreatedFor?.[0].name}</p>
-        //         </Link>
-        //       {/* <p className="mt-2 text-gray-500">Patient: {rooms.User_CreatedFor?.[0].name}</p> */}
-        //          {isDoctor && rooms.User_CreatedFor?.[0]?.id ? (
-        //   <>
-        //     {/* <div className="mb-2 mt-4 flex flex-1 flex-row justify-center">
-        //       <Link href={`/viewNotes/${rooms.id}`}>
-        //         <Button variant={"link"}>Notes</Button>
-        //       </Link>
-        //       <div className="w-2"></div>
-        //       <RecordingModal roomId={rooms.id} />
-        //     </div> */}
-        //   </>
-        // ) : (
-        //   <> </>
-        // )}
-        //     </div>
-        //   </div>
-        // </div>
+    // <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5">
+    //   <div className="mb-2 mt-4 flex flex-1 flex-row justify-center">
+    //     <Link className="flex w-full flex-1 text-sm text-blue-500 hover:underline px-2" href={`/room/${rooms.id}`}>
+    //       Join Session
+    //     </Link>
+    //   </div>
+    //   <button
+    //     // variant={"destructive"}
+    //     onClick={async () => {
+    //       toast({ title: "Deleting Room", description: rooms.title });
+    //       await deleteRoom.mutateAsync({ id: rooms.id });
+    //       await utils.room.invalidate();
+    //     }}
+    //     title="Cancel appointment"
+    //     className="flex w-full flex-1 text-sm text-blue-500 hover:underline px-2"
+    //   >
+    //    Delete/Cancel Session
+    //   </button>
+    //   <div className="p-8 flex items-center">
+    //       <div className="items-start">
+    //       <p className=" pr-4 bg-blue-200 p-2 rounded-lg text-center text-4xl font-bold text-white">{rooms.time.toString().slice(4,11)}</p>
+    //       <Link href={`/viewNotes/${rooms.id}`}>
+    //         <Button variant={"link"}>Notes</Button>
+    //       </Link>
+    //       <RecordingModal roomId={rooms.id} />
+    //       </div>
+
+    //       <div className="mb-2 mt-4 flex flex-1 flex-row justify-center">
+
+    //     </div>
+    //       <div className="ml-4">
+    //        <Button
+    //     variant={"link"}
+    //     onClick={async () => {
+    //       const link = `${window.location.origin}/room/${rooms.id}`;
+    //       await navigator.clipboard.writeText(link);
+    //       toast({ title: "Copied link to clipoard", description: link });
+    //     }}
+    //     className="max-w-full"
+    //   >
+    //     <span className="uppercase tracking-wide text-base text-indigo-500 font-semibold">{rooms.title}</span>
+    //     <LinkIcon height={18} strokeWidth={3} />
+    //   </Button>
+    //       <p className="text-sm ">{moment(rooms.time).local().fromNow()}</p>
+    //       <p className="mt-2 text-gray-500">{rooms.duration} hour(s)</p>
+    //       <p className="mt-2 text-gray-500">Dr. {rooms.User_CreatedBy?.name}, Cardiology Specialist</p>
+    //       {/* <Avatar className="ml-4">
+    //         <AvatarImage src={data?.user.image as string | undefined} alt="User Avatar" />
+    //       </Avatar>               */}
+    //         <Link href={`/viewPatient/${rooms.User_CreatedFor?.[0].id}`} className="mt-2 text-gray-500">
+    //         <p className="mt-2 text-grey-500 hover:text-blue-500" title='click for patient details'>Patient: {rooms.User_CreatedFor?.[0].name}</p>
+    //         </Link>
+    //       {/* <p className="mt-2 text-gray-500">Patient: {rooms.User_CreatedFor?.[0].name}</p> */}
+    //          {isDoctor && rooms.User_CreatedFor?.[0]?.id ? (
+    //   <>
+    //     {/* <div className="mb-2 mt-4 flex flex-1 flex-row justify-center">
+    //       <Link href={`/viewNotes/${rooms.id}`}>
+    //         <Button variant={"link"}>Notes</Button>
+    //       </Link>
+    //       <div className="w-2"></div>
+    //       <RecordingModal roomId={rooms.id} />
+    //     </div> */}
+    //   </>
+    // ) : (
+    //   <> </>
+    // )}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/pages/viewRooms.tsx
+++ src/pages/viewRooms.tsx
@@ -12,37 +12,39 @@ import Header from "~/components/ui/header";
 import { Avatar } from "~/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
 
-
 export default function Home() {
   const { data: createdRoomsPast, isLoading: loading1 } = api.room.getCreatedPast.useQuery();
   const { data: createdRoomsUpcoming, isLoading: loading2 } = api.room.getCreatedUpcoming.useQuery();
   const { data: invitedRoomsPast, isLoading: loading3 } = api.room.getInvitedPast.useQuery();
   const { data: invitedRoomsUpcoming, isLoading: loading4 } = api.room.getInvitedUpcoming.useQuery();
 
-  console.log('roomsreversed', createdRoomsPast?.sort())
-  console.log('roomsforward', createdRoomsPast)
+  const sortedCreatedRoomsUpcoming = createdRoomsUpcoming?.sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
+  const sortedCreatedRoomsPast = createdRoomsPast?.sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
+
+  console.log("roomsreversed", createdRoomsPast?.sort());
+  console.log("roomsforward", createdRoomsPast);
 
   return (
     <>
-
-    
       <Header />
-      <div className="flex flex-col h-screen w-screen overflow-y-scroll bg-gray-100 pb-4 px-10">
-        <h1 className="mb-4 mt-6 flex text-center text-4xl font-sans leading-none text-gray-700">Your Schedule</h1>
-        <h3 className="gray text-center text-base font-sans text-gray-700">Upcoming Appointments</h3>
-        <div className="">
-    
-              <Rooms rooms={createdRoomsUpcoming} isLoading={loading1} />
- 
+      <div className="flex h-screen w-screen flex-col overflow-y-scroll bg-gray-100 px-10 pb-4">
+        <div className="mx-auto max-w-[680px]">
+          <h1 className="mb-4 mt-6 flex text-center font-sans text-4xl leading-none text-gray-700">Your Schedule</h1>
+          <h3 className="gray text-left font-sans text-base text-gray-700">Upcoming Appointments</h3>
+          <div className="">
+            <Rooms rooms={sortedCreatedRoomsUpcoming} isLoading={loading1} />
+          </div>
+          <h3 className="text-left font-sans text-base text-gray-700">Past Appointments</h3>
+          <div>
+            <Rooms rooms={sortedCreatedRoomsPast} isLoading={loading3} />
+          </div>
+
+          <Link href="/">
+            <Button variant={"link"} className="absolute absolute bottom-10 right-10 right-[50px]">
+              back to dashboard
+            </Button>
+          </Link>
         </div>
-        <h3 className="text-center text-base font-sans text-gray-700">Past Appointments</h3>
-        <div>
-              <Rooms rooms={createdRoomsPast?.reverse()} isLoading={loading3} />
-        </div>
-    
-        <Link href="/">
-          <Button variant={"link"} className="absolute right-[50px] absolute bottom-10 right-10">back to dashboard</Button>
-        </Link>
       </div>
       <Footer />
     </>
@@ -52,23 +54,23 @@ export default function Home() {
 const Rooms = ({ rooms, isLoading }: { rooms?: Room[]; isLoading: boolean }) => {
   const { data } = useSession();
   rooms?.forEach((room) => {
-    console.log('room', room)
-  })
+    console.log("room", room);
+  });
   return (
     <div className="my-5 flex flex-col items-center gap-3 rounded-xl">
       {isLoading ? (
-        <Card className="m-8 flex h-[28rem] w-80 flex-col justify-center self-center rounded-lg bg-white p-5 text-center shadow-lg">
+        <Card className="m-8 flex h-[28rem] w-80 min-w-[680px] flex-col justify-center self-center rounded-lg bg-white p-5 text-center shadow-lg">
           <Skeleton className="h-96 w-full animate-pulse" />
         </Card>
       ) : rooms?.length === 0 ? (
-        <Card className="m-4 flex min-h-50 w-64 flex-col justify-center self-center rounded-lg bg-white p-5 text-left shadow-lg">You have no upcoming appointments.</Card>
+        <Card className="min-h-50 m-4 flex w-64 min-w-[680px] flex-col justify-center self-center rounded-lg bg-white p-5 text-left shadow-lg">
+          You have no upcoming appointments.
+        </Card>
       ) : (
         rooms?.map((room) => (
           // <Card key={room.id} className="mx-6 my-4 flex h-[28rem] w-80 flex-col self-center rounded-lg bg-white p-5 text-center shadow-lg">
-          <div className=''>
-
+          <div className="">
             <UpcomingSession data={room} isDoctor={data?.user.role === "doctor"} key={room.id} />
-        
           </div>
         ))
       )}
