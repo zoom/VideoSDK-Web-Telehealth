8c35db5241ca9f6494767bc97dc3fcc5948d9ab0
Ekaansh Arora <ekaansh68@gmail.com>
2024-06-05 21:12:39 +0530

    chore: change terms and privacy

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,25 +1,21 @@
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export default function Tos() {
   return (
     <div className="m-auto my-4 flex max-w-5xl flex-col self-center text-center">
-      <h1 className="text-3xl font-bold text-gray-800">Zoom Privacy Statement</h1>
-      <p className="text-gray-600">Last updated: March 17, 2024</p>
+      <h1 className="text-3xl font-bold text-gray-800">Your Privacy Statement goes here</h1>
       <br />
       <p className="text-justify text-gray-600">
-        This Privacy Statement describes the personal data we collect and/or process (which may include collecting, organizing, structuring, storing, using, or
-        disclosing) to provide products and services offered directly by Zoom Video Communications, Inc. (“Zoom”), including Zoom’s websites, its meetings,
-        webinars, and messaging platform, related collaborative features, and Zoom App Marketplace (“Zoom products and services” or “products and services”).
-        Zoom products and services covered in this Privacy Statement do not include products or services developed by Zoom that are covered under a separate
-        privacy policy (including those listed{" "}
-        <a className="text-blue-500 underline hover:text-blue-600" href="https://explore.zoom.us/en/trust/privacy/policies/">
-          here
-        </a>
-        ).
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed porttitor, nisl a porta ultrices, orci nisi porta nibh, ac porttitor nisl
+        nisi eget nibh. Nullam nec nisl a nisi porta porttitor. Donec nec nulla nec nisi porta porttitor. Nulla facilisi. Sed porttitor, nisl a porta ultrices,
       </p>
       <br />
       <Link href="https://explore.zoom.us/en/privacy/" className="text-blue-500 underline hover:text-blue-600">
-        Read the full privacy policy
+        Read Zoom&apos;s privacy policy for this application
+      </Link>
+      <Link href="/">
+        <Button variant="link">Back</Button>
       </Link>
     </div>
   );
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,27 +1,25 @@
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export default function Tos() {
   return (
     <div className="m-auto my-4 flex max-w-5xl flex-col self-center text-center">
-      <h1 className="text-3xl font-bold text-gray-800">ZOOM TERMS OF SERVICE</h1>
-      <p className="text-gray-600">Effective Date: August 11, 2023</p>
-      <br />
-      <p className="text-justify text-gray-600">
-        Please read these terms of service (“Terms of Service”) and the Zoom Services Description (collectively, this “Agreement”) carefully. This Agreement is
-        between you and Zoom Video Communications, Inc. and its affiliates (“Zoom,” “we,” “us,” or “our”) and governs your access to and use of our Services and
-        Software, unless you and Zoom have entered into a written Master Subscription Agreement (MSA), in which case such MSA will govern your access to and use
-        of the Services and Software and not these Terms of Service. You may enter into this Agreement on behalf of yourself or on behalf of a legal entity. If
-        you enter into this Agreement on behalf of a legal entity, you represent that you are a duly authorized representative with the authority to bind that
-        legal entity to this Agreement. All references to “you” and “your” in this Agreement mean the person accepting this Agreement as an individual or the
-        legal entity for which the representative is acting. Capitalized terms in this Agreement will have definitions as set forth in the applicable section
-        where they are defined, in the Zoom Services Description, or in Section 34 below. We may provide the Services and Software through Zoom Video
-        Communications, Inc., our affiliate(s), or both. You may only use the Services and Software in accordance with the terms and subject to the conditions
-        of this Agreement.
-      </p>
-      <br />
-      <Link className="text-blue-500 underline hover:text-blue-600" href="https://explore.zoom.us/en/terms/">
-        Read the full terms
-      </Link>
+      <div className="m-auto my-4 flex max-w-5xl flex-col self-center text-center">
+        <h1 className="text-3xl font-bold text-gray-800">Your Terms of Service go here</h1>
+        <br />
+        <p className="text-justify text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed porttitor, nisl a porta ultrices, orci nisi porta nibh, ac porttitor nisl
+          nisi eget nibh. Nullam nec nisl a nisi porta porttitor. Donec nec nulla nec nisi porta porttitor. Nulla facilisi. Sed porttitor, nisl a porta
+          ultrices,
+        </p>
+        <br />
+        <Link href="https://explore.zoom.us/en/terms/" className="text-blue-500 underline hover:text-blue-600">
+          Read Zoom&apos;s Terms of Service for this application
+        </Link>
+        <Link href="/">
+          <Button variant="link">Back</Button>
+        </Link>
+      </div>
     </div>
   );
 }
